177bd2afc4d52e68a7d59dbe5360d0e8a08926f7
Taylor Lodge <taylor@boost.co.nz>
2015-11-20 15:24:31 +1300

    Add seeds to setup a basic match

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+[
+  {name: 'Taylor', avatar: 'ignore'},
+  {name: 'Ben', avatar: 'ignore'},
+  {name: 'Gus', avatar: 'ignore'},
+  {name: 'Eddie', avatar: 'ignore'},
+].each{|attributes| Player.create(attributes)}
+
+Match.create(date: Date.current)
+
+[
+  {position: 0, team: 0, goals: 5, match_id: Match.first.id},
+  {position: 1, team: 0, goals: 5, match_id: Match.first.id},
+  {position: 0, team: 1, goals: 4, match_id: Match.first.id},
+  {position: 1, team: 1, goals: 5, match_id: Match.first.id}
+].zip(Player.all).map do |match_player_attributes, player|
+  match_player_attributes.merge({player_id: player.id})
+end.each{|attributes| MatchPlayer.create(attributes)}
