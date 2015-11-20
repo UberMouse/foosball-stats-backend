896a6fe70e7d0314b7a1b9b3736636b286c7b4c3
Taylor Lodge <taylor@boost.co.nz>
2015-11-20 15:39:30 +1300

    Fix IDs in seed data to make for easier graphiql queries

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,19 +7,19 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 [
-  {name: 'Taylor', avatar: 'ignore'},
-  {name: 'Ben', avatar: 'ignore'},
-  {name: 'Gus', avatar: 'ignore'},
-  {name: 'Eddie', avatar: 'ignore'},
+  {id: 1, name: 'Taylor', avatar: 'ignore'},
+  {id: 2, name: 'Ben', avatar: 'ignore'},
+  {id: 3, name: 'Gus', avatar: 'ignore'},
+  {id: 4, name: 'Eddie', avatar: 'ignore'},
 ].each{|attributes| Player.create(attributes)}
 
-Match.create(date: Date.current)
+Match.create(id: 1, date: Date.current)
 
 [
-  {position: 0, team: 0, goals: 5, match_id: Match.first.id},
-  {position: 1, team: 0, goals: 5, match_id: Match.first.id},
-  {position: 0, team: 1, goals: 4, match_id: Match.first.id},
-  {position: 1, team: 1, goals: 5, match_id: Match.first.id}
+  {id: 1, position: 0, team: 0, goals: 5, match_id: 1},
+  {id: 2 ,position: 1, team: 0, goals: 5, match_id: 1},
+  {id: 3, position: 0, team: 1, goals: 4, match_id: 1},
+  {id: 4, position: 1, team: 1, goals: 5, match_id: 1}
 ].zip(Player.all).map do |match_player_attributes, player|
   match_player_attributes.merge({player_id: player.id})
 end.each{|attributes| MatchPlayer.create(attributes)}
