3ccd0b3af74607ac7d66285b6938182ada4354b9
Taylor Lodge <taylor@boost.co.nz>
2015-12-04 09:58:33 +1300

    Looks like GraphQL object types need names, this adds one to QueryType to fix schema parse errors

--- app/graph/types/query_type.rb
+++ app/graph/types/query_type.rb
@@ -1,4 +1,6 @@
 QueryType = GraphQL::ObjectType.define do
+  name "Query"
+
   field :player, PlayerType, field: FetchField.new(model: Player, type: PlayerType)
   field :match, MatchType, field: FetchField.new(model: Match, type: MatchType)
   field :node, field: NodeIdentification.field
