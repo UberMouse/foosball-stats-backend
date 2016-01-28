348a0f09bd489b29fa142bf26052f19b0f9f1ff4
Taylor Lodge <taylor@boost.co.nz>
2016-01-29 12:57:08 +1300

    Validate that :goals is within the range of 0-10

--- app/models/match_player.rb
+++ app/models/match_player.rb
@@ -3,4 +3,5 @@ class MatchPlayer < ActiveRecord::Base
   belongs_to :player
 
   validates_presence_of :match_id, :player_id, :goals
+  validates_numericality_of :goals,  greater_than_or_equal_to: 0, less_than_or_equal_to: 10
 end
--- spec/models/match_player_spec.rb
+++ spec/models/match_player_spec.rb
@@ -3,5 +3,10 @@ RSpec.describe MatchPlayer do
     it {is_expected.to validate_presence_of(:match_id)}
     it {is_expected.to validate_presence_of(:player_id)}
     it {is_expected.to validate_presence_of(:goals)}
+    it do
+      is_expected.to validate_numericality_of(:goals)
+                      .is_greater_than_or_equal_to(0)
+                      .is_less_than_or_equal_to(10)
+    end
   end
 end
