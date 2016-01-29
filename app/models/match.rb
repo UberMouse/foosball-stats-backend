0a6f112511c1cdf35bcec14a979ea4d76d6b017b
Taylor Lodge <taylor@boost.co.nz>
2016-01-29 13:18:22 +1300

    Validate that :date exists

--- app/models/match.rb
+++ app/models/match.rb
@@ -3,4 +3,6 @@ class Match < ActiveRecord::Base
   has_many :players, through: :match_players
 
   accepts_nested_attributes_for :match_players
+
+  validates_presence_of :date
 end
--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -2,5 +2,7 @@ RSpec.describe Match do
   context "model shape" do
     it {is_expected.to have_many(:players)}
     it {is_expected.to accept_nested_attributes_for(:match_players)}
+
+    it {is_expected.to validate_presence_of(:date)}
   end
 end
