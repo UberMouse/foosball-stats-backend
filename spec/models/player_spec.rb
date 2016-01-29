e6ff1c903ea7bb87cd4c62c916ccdf8d29594778
Taylor Lodge <taylor@boost.co.nz>
2016-01-29 13:15:49 +1300

    Validate has_many :matches

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,6 +1,8 @@
 RSpec.describe Player, type: :model do
-  context "validations" do
-    it { is_expected.to validate_presence_of(:name)}
-    it { is_expected.to validate_presence_of(:avatar)}
+  context "model shape" do
+    it {is_expected.to validate_presence_of(:name)}
+    it {is_expected.to validate_presence_of(:avatar)}
+
+    it {is_expected.to have_many(:matches)}
   end
 end
