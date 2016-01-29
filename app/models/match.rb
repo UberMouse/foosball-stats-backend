9967b84083a6b7ff1250db5e18864e5c9a4f0c2c
Taylor Lodge <taylor@boost.co.nz>
2016-01-29 14:32:36 +1300

    Validate a Match has 4 MatchPlayers

--- app/models/match.rb
+++ app/models/match.rb
@@ -5,4 +5,11 @@ class Match < ActiveRecord::Base
   accepts_nested_attributes_for :match_players
 
   validates_presence_of :date
+  validate :number_of_match_players
+
+  def number_of_match_players
+    if match_players.reject(&:marked_for_destruction?).size != 4
+      errors.add(:match_players, "Must have exactly 4 MatchPlayers")
+    end
+  end
 end
--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -4,5 +4,35 @@ RSpec.describe Match do
     it {is_expected.to accept_nested_attributes_for(:match_players)}
 
     it {is_expected.to validate_presence_of(:date)}
+
+    context "number of match_players" do
+      it "is not valid with less than 4 match_players" do
+        match = build(:match, number_of_match_players: 3)
+
+        expect(match).to be_invalid
+        expect(match.errors).to have_key(:match_players)
+      end
+
+      it "is not valid with greater than 4 match_players" do
+        match = build(:match, number_of_match_players: 5)
+
+        expect(match).to be_invalid
+        expect(match.errors).to have_key(:match_players)
+      end
+
+      it "is not valid if a match_player is marked_for_destruction" do
+        match = build(:match, number_of_match_players: 4)
+        match.match_players.first.mark_for_destruction
+
+        expect(match).to be_invalid
+        expect(match.errors).to have_key(:match_players)
+      end
+
+      it "is valid with exactly 4 match_players" do
+        match = build(:match, number_of_match_players: 4)
+
+        expect(match).to be_valid
+      end
+    end
   end
 end
